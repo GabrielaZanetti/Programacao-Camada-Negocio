b7c90b78a40ed6a795a7049997f59db23c105cd5
GabrielaZanetti <gabiisctt@gmail.com>
2025-02-21 22:09:08 -0300

    Aula 1

---
+++ PCN/src/aula1/aula1.java
@@ -0,0 +1,19 @@
+package aula1;
+
+/**
+ *
+ * @author gabriela.zanetti
+ */
+public class aula1 {
+
+    public static void main(String[] args) {
+        byte b1 = (byte) 10;
+        System.out.print("b1: " +b1);
+        
+        // Byte b10 = new Byte((byte) 10); - nao e mais utilizado desta maneira
+        Byte b10 = ((byte) 10);
+        System.out.print("b10: " +b10);
+
+    }
+
+}
