bd57229791fa05116868e924d80a381c54824fdb
Gabriela da Rosa Zanetti <70355117+GabrielaZanetti@users.noreply.github.com>
2025-04-25 21:54:44 -0300

    aula 7 sql

---
+++ aulas/src/aula8/BasicCrud/Main.java
@@ -0,0 +1,124 @@
+package aula8.BasicCrud;
+
+import aula8.BasicCrud.Model.Client;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.ResultSet;
+
+public class Main {
+
+    static final String DATABASE_URL = "jdbc:derby://localhost:1527/siscom";
+    static final String USER = "app";
+    static final String PASSWORD = "app";
+    
+    public static void main(String[] args) {
+       /* Insere um cliente novo
+       Client client = new Client("ana", "Rua um", "55 9999-9999", "juliana@email.com");
+       insert(client);
+       */
+
+       /* retorna a lista de clientes
+       List<Client> list = getAll();
+       for (Client item: list) {
+        System.out.println(item);
+       }
+       */
+       
+       // Busca apenas um id
+       // System.out.println(getByID(1));
+       
+       /* Deletar um item */
+    }
+
+    public static void insert(Client client) {
+        String sql = "INSERT INTO clientes (nome, endereco, telefone, email) VALUES (?,?,?,?)";
+
+        try (Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD)) {
+            PreparedStatement smtm = connection.prepareStatement(sql);
+
+            smtm.setString(1, client.getName());
+            smtm.setString(2, client.getAddress());
+            smtm.setString(3, client.getPhone());
+            smtm.setString(4, client.getEmail());
+
+            smtm.execute();
+            connection.commit();
+
+        } catch (SQLException ex) {
+            System.out.println("erro: " + ex.getMessage());
+        }
+    }
+    
+    public static List<Client> getAll() {
+        List<Client> clients = new ArrayList<>();
+        String sql = "Select * FROM CLIENTES";
+
+        try (Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+             PreparedStatement select_clients = connection.prepareStatement(sql);) {
+            
+            ResultSet result = select_clients.executeQuery();
+            while (result.next()) {
+                Client client = new Client();
+                client.setId(result.getInt("id"));
+                client.setName(result.getString("nome"));
+                client.setPhone(result.getString("telefone"));
+                client.setAddress(result.getString("endereco"));
+                client.setEmail(result.getString("email"));
+                
+                clients.add(client);
+            }   
+        } catch (SQLException ex) {
+            System.out.println("erro: " + ex.getMessage());
+        }
+        
+        return clients;
+    }
+    
+    public static Client getByID(int id) {
+        Client client = new Client();
+        String sql = "Select * FROM CLIENTES WHERE id = ?";
+
+        try (Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+             PreparedStatement select_clients = connection.prepareStatement(sql);) {
+            select_clients.setInt(1, id);
+            ResultSet result = select_clients.executeQuery();
+            if (result.next()) {
+                client.setId(result.getInt("id"));
+                client.setName(result.getString("nome"));
+                client.setPhone(result.getString("telefone"));
+                client.setAddress(result.getString("endereco"));
+                client.setEmail(result.getString("email"));
+            }   
+        } catch (SQLException ex) {
+            System.out.println("erro: " + ex.getMessage());
+        }
+        
+        return client;
+    }
+    
+    public static Client Delet(int id) {
+        Client client = new Client();
+        String sql = "Select * FROM CLIENTES WHERE id = ?";
+
+        try (Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+             PreparedStatement select_clients = connection.prepareStatement(sql);) {
+            select_clients.setInt(1, id);
+            ResultSet result = select_clients.executeQuery();
+            if (result.next()) {
+                client.setId(result.getInt("id"));
+                client.setName(result.getString("nome"));
+                client.setPhone(result.getString("telefone"));
+                client.setAddress(result.getString("endereco"));
+                client.setEmail(result.getString("email"));
+            }   
+        } catch (SQLException ex) {
+            System.out.println("erro: " + ex.getMessage());
+        }
+        
+        return client;
+    }
+}
