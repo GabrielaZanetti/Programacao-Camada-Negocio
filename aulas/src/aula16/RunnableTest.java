8889e2028201ddebf924071883bd47ba4097b82b
Gabriela da Rosa Zanetti <70355117+GabrielaZanetti@users.noreply.github.com>
2025-06-20 20:44:58 -0300

    verificacao do tempo de execucao

--- aulas/src/aula16/RunnableTest.java
+++ aulas/src/aula16/RunnableTest.java
@@ -7,14 +7,23 @@ package aula16;
 public class RunnableTest {
 
     public static void main(String[] args) throws InterruptedException {
-        int[] numeros = {1,2,3,4,5};
-        MyThreadRunnable r1 = new MyThreadRunnable(numeros);
+        int[] vetor = new int[1000];
+        
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = i;
+        }
+        
+        MyThreadRunnable r1 = new MyThreadRunnable(vetor);
         Thread t1 = new Thread(r1);
+        
+        long start = System.nanoTime();
         t1.start();
         // se nao executa primeiro ele retorna a soma 0 por nao finalizar a soma
         t1.join();
-
+        long end = System.nanoTime();
+        
         System.out.println("Valor da soma: "+ r1.getSoma());
+        System.out.println("Tempo de duracao "+ (end-start));
     }
 
 }
