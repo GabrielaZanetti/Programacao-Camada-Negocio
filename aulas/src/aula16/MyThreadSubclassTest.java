8342e01f42f74ac20e46fdd659a357e7fcee7856
Gabriela da Rosa Zanetti <70355117+GabrielaZanetti@users.noreply.github.com>
2025-06-20 20:13:40 -0300

    da prioridade com join e settime para aguardar

--- aulas/src/aula16/MyThreadSubclassTest.java
+++ aulas/src/aula16/MyThreadSubclassTest.java
@@ -1,29 +1,44 @@
 package aula16;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author gabriela.zanetti
  */
 class MyThreadSubClass extends Thread {
+    private int timeToSlep;
 
+    public MyThreadSubClass(int timeToSlep) {
+        this.timeToSlep = timeToSlep;
+    }
+            
     @Override
     public void run () {
         String threadName = Thread.currentThread().getName();
         System.out.println("Executado a thread: " + Thread.currentThread().getName());
         
         for (int i = 0; i < 10; i++) {
-            System.out.println(threadName + " " + i);
+            
+            try {
+                System.out.println(threadName + " " + i);
+                // aguardo no start das thread
+                Thread.sleep(timeToSlep);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(MyThreadSubClass.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
         }
-        
         
     }
 }
 
 public class MyThreadSubclassTest {
 
-    public static void main(String[] args) {
-        MyThreadSubClass t1 = new MyThreadSubClass();
-        MyThreadSubClass t2 = new MyThreadSubClass();
+    public static void main(String[] args) throws InterruptedException {
+        MyThreadSubClass t1 = new MyThreadSubClass(100);
+        MyThreadSubClass t2 = new MyThreadSubClass(300);
         
         t1.setName("thread_01");
         t2.setName("\t\tthread_02");
@@ -35,7 +50,11 @@ public class MyThreadSubclassTest {
         // executa na thread main
         // t1.run();
         // t2.run();;
-
+        
+        // da prioridade para a t1
+        t1.join();
+        t2.join();
+        
         // metodo main executa dentro da thread principal main
         System.out.println("Fim da " + Thread.currentThread().getName());               
     }
